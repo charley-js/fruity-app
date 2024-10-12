9541b07e6055e586c3743b0708121708da9700a2
Charley Crowson <charleycrowson1997@gmail.com>
2024-10-12 17:38:17 +0100

    feat: add phaser preload function and preload fruit symbols - add phaser preload function to phaser config - insert fruit symbol images into assets folder - preload fruit symbols in preload function

--- js/app.js
+++ js/app.js
@@ -4,6 +4,18 @@ const config = {
   height: 500,
   parent: "container",
   backgroundColor: "#283048",
+  scene: {
+    preload,
+  },
 };
 
 const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image("apple", "../assets/images/apple.png");
+  this.load.image("banana", "../assets/images/banana.png");
+  this.load.image("cherries", "../assets/images/cherries.png");
+  this.load.image("lemon", "../assets/images/lemon.png");
+  this.load.image("strawberry", "../assets/images/strawberry.png");
+  this.load.image("watermelon", "../assets/images/watermelon.png");
+}
